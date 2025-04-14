a6b23a853b6c1b834e9f277203f86075a59ad13f
Jonas Alves <jonasalvesbueno@gmail.com>
2025-04-14 08:00:28 -0300

    first commit

---
+++ src/main/java/br/gov/sp/fatec/BFJPE/BestFckingJavaProjectEverApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.BFJPE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestFckingJavaProjectEverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestFckingJavaProjectEverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/BFJPE/BestFckingJavaProjectEverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.BFJPE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestFckingJavaProjectEverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
